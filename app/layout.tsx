1b62fcf62896005095416bb5ac2f5516d267d98d
chenhaojie <a18520145493@163.com>
2023-12-12 11:20:10 +0800

    feat: 配置路由组

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AI Saas Shop",
+  description: "这是一个AI Saas 商店",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
