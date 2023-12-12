9f82c97738981e376886390e420766b87979d8a3
chenhaojie <a18520145493@163.com>
2023-12-12 18:04:08 +0800

    feat: 登录、注册按钮

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function LandingPage() {
-  return <div>Lading Page...</div>;
+  return (
+    <div>
+      Lading Page...
+      <div>
+        <Link href="/sign-in">
+          <Button>登录</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>注册</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
