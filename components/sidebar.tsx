56dc72504ad80a0a440fa822e832ae75cce928c6
chenhaojie <a18520145493@163.com>
2023-12-14 18:01:17 +0800

    feat: logo文案样式

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,13 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  weight: "600",
+  subsets: ["latin"],
+});
 
 export default function Sidebar() {
   return (
@@ -10,7 +17,9 @@ export default function Sidebar() {
           <div className="relative w-8 h-8 mr-4">
             <Image fill alt="logo" src={"/logo.png"} />
           </div>
-          <h1 className="font-bold text-2xl">AI Saas</h1>
+          <h1 className={cn("font-bold text-2xl", montserrat.className)}>
+            AI Saas
+          </h1>
         </Link>
       </div>
     </div>
